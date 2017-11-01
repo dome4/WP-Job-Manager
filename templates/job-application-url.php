3141f2167dade3fab5111dfee6ab2610ac39cd7f
dome4 <dominic.seitz@gmx.de>
2017-11-01 19:43:51 +0100

    changed application text

--- templates/job-application-url.php
+++ templates/job-application-url.php
@@ -15,4 +15,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<p><?php _e( 'To apply for this job please visit the following URL:', 'wp-job-manager' ); ?> <a href="<?php echo esc_url( $apply->url ); ?>" target="_blank" rel="nofollow"><?php echo esc_html( $apply->url ); ?> &rarr;</a></p>
+<p><?php echo 'Um Dich für diesen Job zu bewerben, besuche bitte die folgende Website: '; ?> <a href="<?php echo esc_url( $apply->url ); ?>" target="_blank" rel="nofollow"><?php echo esc_html( $apply->url ); ?> &rarr;</a></p>
